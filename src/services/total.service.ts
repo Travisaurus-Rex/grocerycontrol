ac27aa89704650c800bc7b80aa96efb8c0c3f471
Travis Adams <brokenCD@live.com>
2025-09-24 08:41:39 +0700

    add return types and fix indentation in total service

--- src/services/total.service.ts
+++ src/services/total.service.ts
@@ -8,19 +8,19 @@ export class TotalService {
 
 	constructor(public storage: Storage) {}
 
-  resetTotal() {
-    this.storage.set('grocery_list_total', '0');
-  }
+	resetTotal(): void {
+		this.storage.set('grocery_list_total', '0');
+	}
 
-	setTotal(data) {
-    this.storage.set('grocery_list_total', data);
-  }
+	setTotal(data): void {
+		this.storage.set('grocery_list_total', data);
+	}
 
 	getTotal(): number {
     	this.storage.get('grocery_list_total')
-      		.then(totalString => {
-        		if (totalString != null) this.total = JSON.parse(totalString);
-      		})
-      return this.total;
-  }
+			.then((totalString: string) => {
+				if (totalString) this.total = JSON.parse(totalString);
+			})
+      	return this.total;
+  	}
 }
